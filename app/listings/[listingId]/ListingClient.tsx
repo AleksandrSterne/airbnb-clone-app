2d2f155f54ce661d39cc78543d676a4a9c0655c8
AleksandrSterne <lazdju@gmail.com>
2023-04-18 12:49:50 +0200

    Added routes and logic to reservation functionality

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -6,8 +6,7 @@ import ListingInfo from '@/app/components/listings/ListingInfo';
 import ListingReservation from '@/app/components/listings/ListingReservation';
 import { categories } from '@/app/components/navbar/Categories';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { SafeListing, SafeUser } from '@/app/types';
-import { Reservation } from '@prisma/client';
+import { SafeListing, SafeReservation, SafeUser } from '@/app/types';
 import axios from 'axios';
 import { differenceInCalendarDays, eachDayOfInterval } from 'date-fns';
 import { useRouter } from 'next/navigation';
@@ -22,7 +21,7 @@ const initialDateRange = {
 };
 
 interface ListingClientProps {
-    reservations?: Reservation[];
+    reservations?: SafeReservation[];
     listing: SafeListing & {
         user: SafeUser;
     };
@@ -63,7 +62,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
         setIsLoading(true);
 
         axios
-            .post('/api/reservatons', {
+            .post('/api/reservations', {
                 totalPrice,
                 startDate: dateRange.startDate,
                 endDate: dateRange.endDate,
@@ -113,7 +112,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
                         id={listing.id}
                         currentUser={currentUser}
                     />
-                    <div className="grid mt-6 grid-col-1 md:grid-col-7 md:gap-10">
+                    <div className="grid grid-cols-1 mt-6 md:grid-cols-7 md:gap-10">
                         <ListingInfo
                             user={listing.user}
                             category={category}
